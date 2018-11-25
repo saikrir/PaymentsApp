7442fc2405d45332b7581fe4831d4a72d595ac8d
Sai Katterishetty <rao.saikrishna@gmail.com>
2018-11-25 00:02:38 -0600

    Added Perisitance Layer

--- api/src/main/resources/sql/ddl/createSchema.sql
+++ api/src/main/resources/sql/ddl/createSchema.sql
@@ -2,6 +2,14 @@ CREATE DATABASE `MyPayments_TEST`;
 
 USE MyPayments_TEST;
 
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `emailId` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
 CREATE TABLE `Product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
@@ -10,11 +18,15 @@ CREATE TABLE `Product` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
-CREATE TABLE `Product_User` (
+CREATE TABLE `User_Products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `emailId` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_User_Products_1_idx` (`user_id`),
+  KEY `fk_User_Products_2_idx` (`product_id`),
+  CONSTRAINT `fk_User_Products_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_User_Products_2` FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -29,5 +41,6 @@ CREATE TABLE `User_Product_Payment` (
   KEY `fk_User_Product_Payment_1_idx` (`product_id`),
   KEY `fk_User_Product_Payment_2_idx` (`paid_by_user`),
   CONSTRAINT `fk_User_Product_Payment_1` FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_User_Product_Payment_2` FOREIGN KEY (`paid_by_user`) REFERENCES `Product_User` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_User_Product_Payment_2` FOREIGN KEY (`paid_by_user`) REFERENCES `User` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
